e7d2a1556df25f325c88bd9f436e850fae02f8f3
odin <odin.servely@hotmail.fr>
2018-07-02 23:48:26 +0200

    MAJ

--- src/main/webapp/app/features/create-association/create-association.component.ts
+++ src/main/webapp/app/features/create-association/create-association.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Principal} from 'app/core';
+import {AssociationService} from 'app/entities/association';
+import {ProfileService} from 'app/entities/profile';
+import {Profile} from 'app/shared/model/profile.model';
+import {Association} from 'app/shared/model/association.model';
 
 @Component({
   selector: 'jhi-create-association',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAssociationComponent implements OnInit {
 
-  constructor() { }
+    account: Account;
+    profile: Profile = new Profile();
+    association: Association = new Association();
+
+    constructor(
+      private principal: Principal,
+      private associationService: AssociationService,
+      private profileService: ProfileService,
+  ) { }
 
   ngOnInit() {
+      this.principal.identity().then( account => {
+          this.account = account;
+
+      });
   }
 
 }
